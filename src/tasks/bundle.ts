3dfe10bf1bdbe258008c63ad18a8c8850e448fed
Netanel Gilad <netanelgilad@gmail.com>
2016-11-18 08:32:46 +0200

    fix bundle task to return a promise

--- src/tasks/bundle.ts
+++ src/tasks/bundle.ts
@@ -3,10 +3,20 @@ import {onBundleCompleted} from "../utils/onBundleCompleted";
 import {log} from "gulp-util";
 
 export function bundle(options) {
-  const compiler = createCompiler(options);
+  return new Promise((resolve, reject) => {
+    const compiler = createCompiler(options);
 
-  log('------------------------------------------');
-  log('===> Creating bundle...');
-  log('------------------------------------------');
-  compiler.run(onBundleCompleted);
+    log('------------------------------------------');
+    log('===> Creating bundle...');
+    log('------------------------------------------');
+    compiler.run((err, stats) => {
+      onBundleCompleted(err, stats);
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(stats);
+      }
+    });
+  })
 }
